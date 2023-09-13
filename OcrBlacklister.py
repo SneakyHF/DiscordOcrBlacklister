bf6f2511f9be6a80c03d84af41745e0a43ab358e
$neaky <135932795+SneakyHF@users.noreply.github.com>
2023-09-13 18:11:38 +0200

    Update OcrBlacklister.py

--- OcrBlacklister.py
+++ OcrBlacklister.py
@@ -28,76 +28,14 @@ def perform_ocr(image):
         print(f"Error performing OCR: {e}")
     return None
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    for attachment in message.attachments:
-        if attachment.content_type.startswith('image'):
-            await process_image_attachment(message, attachment)
-
-    image_links = find_image_links(message.content)
-    for link in image_links:
-        await process_linked_image(message, link)
-
-    if is_website_link(message.content):
-        await process_website_link(message)
-
-    await bot.process_commands(message)
-
-async def process_image_attachment(message, attachment):
-    try:
-        image_bytes = await attachment.read()
-        image = Image.open(io.BytesIO(image_bytes))
-        
-        extracted_text = perform_ocr(image)
-        print(f"Extracted text from image: {extracted_text}")
-
-        for phrase in BLACKLISTED_PHRASES:
-            if phrase.lower() in extracted_text.lower():
-                await handle_blacklisted_message(message)
-                return
-    except Exception as e:
-        print(f"Error processing image attachment: {e}")
-
-async def process_linked_image(message, link):
-    try:
-        print(f"Processing linked image: {link}")
-        response = requests.get(link)
-
-        if response.status_code == 200:
-            print(f"Image URL: {link}")
-
-            extracted_text = perform_ocr(Image.open(io.BytesIO(response.content)))
-
-            if extracted_text:
-                print(f"Extracted text from linked image: {extracted_text}")
-
-                for phrase in BLACKLISTED_PHRASES:
-                    if phrase.lower() in extracted_text.lower():
-                        await handle_blacklisted_message(message)
-                        return
-            else:
-                print(f"Error: Failed to extract text from linked image: {link}")
-        else:
-            print(f"Error: Received status code {response.status_code} for linked image: {link}")
-    except Exception as e:
-        print(f"Error processing linked image: {e}")
+def find_urls(text):
+    return re.findall(r'(https?://\S+)', text)
 
 def find_image_links(text):
     return re.findall(r'(https?://\S+\.(?:png|jpg|jpeg|gif|bmp|tiff))', text)
 
-def is_website_link(text):
-    return text.startswith(('http://', 'https://'))
-
-async def process_website_link(message):
+async def process_url(message, url):
     try:
-        url = message.content
         for phrase in BLACKLISTED_PHRASES:
             if phrase.lower() in url.lower():
                 await handle_blacklisted_message(message)
@@ -114,7 +52,64 @@ async def process_website_link(message):
                     await handle_blacklisted_message(message)
                     return
     except Exception as e:
-        print(f"Error processing website link: {e}")
+        print(f"Error processing URL: {e}")
+
+async def process_image_attachment(message, attachment):
+    try:
+        image_bytes = await attachment.read()
+        image = Image.open(io.BytesIO(image_bytes))
+        
+        extracted_text = perform_ocr(image)
+        print(f"Extracted text from image: {extracted_text}")
+
+        await check_blacklisted_content(message, extracted_text)
+    except Exception as e:
+        print(f"Error processing image attachment: {e}")
+
+async def process_linked_images(message, image_links):
+    for link in image_links:
+        try:
+            print(f"Processing linked image: {link}")
+            response = requests.get(link)
+
+            if response.status_code == 200:
+                print(f"Image URL: {link}")
+
+                extracted_text = perform_ocr(Image.open(io.BytesIO(response.content)))
+
+                if extracted_text:
+                    print(f"Extracted text from linked image: {extracted_text}")
+
+                    await check_blacklisted_content(message, extracted_text)
+                else:
+                    print(f"Error: Failed to extract text from linked image: {link}")
+            else:
+                print(f"Error: Received status code {response.status_code} for linked image: {link}")
+        except Exception as e:
+            print(f"Error processing linked image: {e}")
+
+async def check_blacklisted_content(message, content):
+    for phrase in BLACKLISTED_PHRASES:
+        if phrase.lower() in content.lower():
+            await handle_blacklisted_message(message)
+            return
+
+    for embed in message.embeds:
+        if embed.description:
+            if any(phrase.lower() in embed.description.lower() for phrase in BLACKLISTED_PHRASES):
+                await handle_blacklisted_message(message)
+                return
+
+    for attachment in message.attachments:
+        if attachment.content_type.startswith('image'):
+            await process_image_attachment(message, attachment)
+
+    image_links = find_image_links(content)
+    await process_linked_images(message, image_links)
+    
+    urls = find_urls(content)
+    for url in urls:
+        await process_url(message, url)
 
 async def handle_blacklisted_message(message):
     if message.id not in removed_messages:
@@ -126,5 +121,23 @@ async def handle_blacklisted_message(message):
         async for own_message in message.channel.history():
             if own_message.author == bot.user and own_message.id != message.id:
                 await own_message.delete()
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await check_blacklisted_content(message, message.content)
+
+@bot.event
+async def on_message_edit(before, after):
+    if before.author == bot.user:
+        return
+
+    await check_blacklisted_content(after, after.content)
 
 bot.run(TOKEN)
